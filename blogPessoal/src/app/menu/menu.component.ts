bf2a87f688d374a601963e02021220b682e80821
Lucas <Lucas@D9997NE038.edu.fb.org.br>
2021-01-19 21:47:19 -0300

    Proteção do link de imagens através de TypeScript

--- blogPessoal/src/app/menu/menu.component.ts
+++ blogPessoal/src/app/menu/menu.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-
+  public urlImgMenu = "../../assets/img/download.png";
+  
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 }
