308ef81cd49588e250719651ad1779eaa4dd30d0
Lucas Gabriel <lucas.gato.03092001@gmail.com>
2021-01-29 11:54:29 -0300

    Cadastro e Listagem, POST e GET, respectivamente

---
+++ blogPessoal/src/app/service/tema.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { Temas } from '../model/Temas';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class TemaService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
+  
+  getAllTemas(): Observable<Temas[]>{
+    return this.http.get<Temas[]>('http://localhost:8080/temas', this.token)
+  }
+
+  postTemas(tema: Temas): Observable<Temas>{
+    return this.http.post<Temas>('http://localhost:8080/temas', tema, this.token)
+  }
+
+  
+}
--- blogPessoal/src/app/tema/tema.component.ts
+++ blogPessoal/src/app/tema/tema.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Temas } from '../model/Temas';
+import { TemaService } from '../service/tema.service'
 
 @Component({
   selector: 'app-tema',
@@ -8,16 +10,38 @@ import { environment } from 'src/environments/environment.prod';
   styleUrls: ['./tema.component.css']
 })
 export class TemaComponent implements OnInit {
-
+  tema: Temas = new Temas()
+  listaTemas: Temas[]
+  
   constructor(
-   private routh: Router
+   private routh: Router,
+   private temaService: TemaService
   ) { }
 
   ngOnInit(){
-    // if(environment.token == ''){
-    //   alert("Sua seção expirou, por favor, faça o login novamente!")
-    //   this.routh.navigate(["/entrar"])
-    // }
+    if(environment.token == ''){
+      alert("Sua seção expirou, por favor, faça o login novamente!")
+      this.routh.navigate(["/entrar"])
+    }
+
+    this.findAllTemas()
+  }
+  findAllTemas(){
+    this.temaService.getAllTemas().subscribe((resp: Temas[]) =>
+    this.listaTemas = resp
+    )
+  }
+
+  cadastrar(){
+    
+
+    this.temaService.postTemas(this.tema).subscribe((resp: Temas) => {
+      this.tema = resp
+      alert('Tema cadastrado com sucesso!')
+      this.findAllTemas()
+      this.tema = new Temas()
+    })
+
   }
 
 }
