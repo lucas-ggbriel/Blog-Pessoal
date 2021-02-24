7a6b1da55115dab0a6ae6554411150cd6c6856bd
Lucas Gabriel <lucas.gato.03092001@gmail.com>
2021-02-24 15:14:52 -0300

    Inserção dos endpoint de edição e deleção

--- blogpessoaldeploy/blog-pessoalapi/src/main/java/com/generation/Blog/Pessoal/controller/UsuarioController.java
+++ blogpessoaldeploy/blog-pessoalapi/src/main/java/com/generation/Blog/Pessoal/controller/UsuarioController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,29 +25,37 @@ import com.generation.Blog.Pessoal.service.UsuarioService;
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UsuarioController {
 
-	
 	@Autowired
 	private UsuarioService usuarioService;
-	
+
 	@Autowired
 	private UsuarioRepository usuarioRepository;
-	
+
 	@GetMapping("/{id}")
-	public ResponseEntity<Usuario> usuarioPostagem(@PathVariable long id){
+	public ResponseEntity<Usuario> usuarioPostagem(@PathVariable long id) {
 		return ResponseEntity.ok(usuarioRepository.findUsuarioById(id));
 	}
-	
+
 	@PostMapping("/logar")
-	public ResponseEntity<UserLogin> Autentication(@RequestBody  Optional<UserLogin> user){
+	public ResponseEntity<UserLogin> Autentication(@RequestBody Optional<UserLogin> user) {
 		return usuarioService.logar(user).map(resp -> ResponseEntity.ok(resp))
 				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
 	}
-	
+
 	@PostMapping("/cadastrar")
-	public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario){
-		return ResponseEntity.status(HttpStatus.CREATED)
-				.body(usuarioService.CadastrarUsuario(usuario));
-		
+	public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.CadastrarUsuario(usuario));
+
 	}
+
+	@PutMapping
+	public ResponseEntity<Usuario> putUser(Usuario user) {
+		return ResponseEntity.status(HttpStatus.OK).body(usuarioRepository.save(user));
+	}
+
+	@DeleteMapping("/deletar/{id}")
+	public void deletar(@PathVariable long id) {
+		usuarioRepository.deleteById(id);
+	}
+
 }
-		
