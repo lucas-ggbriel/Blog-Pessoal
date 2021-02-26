60be8fe49218eef1f06d70583b20c6dae04b314d
Lucas Gabriel <lucas.gato.03092001@gmail.com>
2021-02-26 07:30:18 -0300

    Get usuário by Id

--- blogpessoaldeploy/blog-pessoalapi/src/main/java/com/generation/Blog/Pessoal/controller/UsuarioController.java
+++ blogpessoaldeploy/blog-pessoalapi/src/main/java/com/generation/Blog/Pessoal/controller/UsuarioController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,7 +35,6 @@ public class UsuarioController {
 	public ResponseEntity<Usuario> usuarioPostagem(@PathVariable long id){
 		return ResponseEntity.ok(usuarioRepository.findUsuarioById(id));
 	}
-	
 	@PostMapping("/logar")
 	public ResponseEntity<UserLogin> Autentication(@RequestBody  Optional<UserLogin> user){
 		return usuarioService.logar(user).map(resp -> ResponseEntity.ok(resp))
@@ -45,7 +45,7 @@ public class UsuarioController {
 	public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario){
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body(usuarioService.CadastrarUsuario(usuario));
-		
 	}
+	
 }
 		
